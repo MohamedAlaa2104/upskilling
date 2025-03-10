85630fd8b2360b8aeac0325f93e017620832faaf
Mohamed Aladdin <mohamed@Mohameds-MacBook-Air.local>
2025-03-10 23:46:12 +0200

    Session one

---
+++ 1-session one/index.php
@@ -0,0 +1,162 @@
+<?php
+
+// Declaration for name variable
+// $name = "Ahmed";
+
+//echo $name;
+
+# Comment
+/*
+ * Line one
+ * Line two
+ * Line three
+ * Line four
+ */
+
+
+// String Data Type
+
+//$name = "John Doe";
+
+
+// Integer Data Type
+//$age = 20;
+//$age = 0;
+
+
+// Float Data Type
+//$price = -15.5;
+
+// Boolean Data Type
+//$value = true;
+
+
+// Array Data Type
+// Indexed Array
+//$value = [1,2, 'Ahmed', 12.5, true];
+// echo $value[2];
+
+
+// Associative Array
+//$value = [
+//    'name'     => 'Ahmed',
+//    'age'      => 20,
+//    'location' => 'Egypt'
+//];
+//
+//$value['name'] = 'Osama';
+//$value['mobile'] = '0123456789';
+
+//$product1 = [
+//    'name'     => 'Product 1',
+//    'price'    => 100,
+//    'quantity' => 10,
+//    'items' => [
+//        [
+//            'name'     => 'Item 1',
+//            'size'    => 45,
+//            'colors' => [
+//                'red',
+//                'green',
+//                'blue'
+//            ]
+//        ]
+//    ]
+//];
+//
+//$product2 = [
+//    'name'     => 'Product 2',
+//    'price'    => 200,
+//    'quantity' => 20
+//];
+//
+//$product3 = [
+//    'name'     => 'Product 3',
+//    'price'    => 300,
+//    'quantity' => 30
+//];
+//
+//$product4 = [
+//    'name'     => 'Product 4',
+//    'price'    => 400,
+//    'quantity' => 40
+//];
+//
+//// Multi-Dimensional Array
+//$productsArray = [];
+//
+//$productsArray[] = $product1;
+
+
+
+//Constants
+
+//define('DAYS_OF_WEEK', 7);
+//
+//
+//echo DAYS_OF_WEEK;
+
+//const PI = 3.14;
+//echo PI;
+//
+//const PI = 5;
+
+//
+//$name = 'test';
+//
+//$$name = 'value';
+//
+//echo $test;
+
+//$name = null;
+//
+//echo $name;
+
+
+//$firstName = 'Ahmed';
+//$lastName = 'Osama';
+//
+//echo "$firstName $lastName";
+
+//$firstNumber = 10;
+//$secondNumber = 2;
+//$thirdNumber = 3;
+
+//echo "First number + second number = " . $firstNumber + $secondNumber . "<br>";
+//echo "First number - second number = " . $firstNumber - $secondNumber . "<br>";
+//echo "First number * second number = " . $firstNumber * $secondNumber . "<br>";
+//echo "First number / second number = " . $firstNumber / $secondNumber . "<br>";
+//
+//echo "First number % second number = 10 % 2 = " . $firstNumber % $secondNumber . "<br>";
+
+//echo "(10 + 2) / 3 = " . (($firstNumber + $secondNumber) / $thirdNumber) . "<br>";
+
+
+// If Condition
+//$array = [1,2,3,4,5];
+//
+//if($array[2] == 4){
+//    echo "Condition is true";
+//}else{
+//    echo "Condition is false";
+//}
+
+// integers 0 , -0 = false
+// floats 0.0 = false
+// strings "" = false
+// strings "0" = false
+//array [] = false
+//null = false
+//$number = 5;
+//
+//if ($number > 7) {
+//    echo "Number is greater than 7";
+//}elseif ($number > 4)
+//    echo "Number is greater than 4";
+//elseif ($number > 1){
+//    echo "Number is less than 1";
+//}else{
+//   echo "Number is less than 1";
+//}
+
+
